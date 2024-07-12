734a36f4dde93a8b8ecb221fc9196a0133d5d95a
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-07-12 16:54:05 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -2,4 +2,12 @@ import re
 
 def add(num):
   answer = num.split(',')
-  print(answer)
+  
+    def find_numeric_values_with_regex(answer):
+      # Use regular expression to find all numeric values
+        numeric_values = re.findall(r'\d+', s)
+        return numeric_values
+
+    for test_string in answer:
+        numeric_values = find_numeric_values_with_regex(test_string)
+        print(f"Numeric values in '{test_string}': {numeric_values}")
