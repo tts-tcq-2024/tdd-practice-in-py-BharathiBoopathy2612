57722345e16c37784be4d2fd87834ef69c082c71
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-07-12 18:55:47 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -6,10 +6,10 @@ def add(num):
     # Example usage
     test_strings = answers
     return answers
-    def find_numeric_values_with_regex(s):
-         # Use regular expression to find all numeric values
-         numeric_values = re.findall(r'\d+', s)
-         return numeric_values
+def find_numeric_values_with_regex(s):
+     # Use regular expression to find all numeric values
+     numeric_values = re.findall(r'\d+', s)
+     return numeric_values
 
 for test_string in test_strings:
     numeric_values = find_numeric_values_with_regex(test_string)
