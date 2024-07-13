5615cdbbfae8acce3bea04b04707e7e152da82f5
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-07-13 12:59:49 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,11 +1,11 @@
 import re
 
 def sum_numeric_values_with_limit_regex(s, limit=1000):
-    # Use regular expression to find all numeric values
+    # Use regular expression 
     numeric_values = re.findall(r'\d+', s)
     numeric_sum = 0
 
-    # Convert numeric strings to integers and sum them with a limit
+    # Convert numeric strings 
     for value in numeric_values:
         num = int(value)
         if numeric_sum + num > limit:
